0effb633ffdfc9da6a548786308bbaa7e3a3fce4
meiLNguyen <bayxanao@gmail.com>
2024-06-23 22:13:54 +0700

    customer-list

--- demo/src/main/java/com/hutech/demo/SecurityConfig.java
+++ demo/src/main/java/com/hutech/demo/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/js/**", "/", "/oauth/**", "/register", "/error", "/products", "/cart", "/cart/**")
                         .permitAll() // Cho phép truy cập không cần xác thực.
-                        .requestMatchers("/products/edit/**", "/products/add", "/products/delete")
+                        .requestMatchers("/products/edit/**", "/products/add", "/products/delete","/customers")
                         .hasAnyAuthority("ADMIN") // Chỉ cho phép ADMIN truy cập.
                         .requestMatchers("/api/**")
                         .permitAll() // API mở cho mọi người dùng.
